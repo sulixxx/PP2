6e5e944364e1a3865019fbaa01cde57954942ed3
Zakariya Sultan <146107268+sulixxx@users.noreply.github.com>
2024-01-20 18:04:16 +0600

    Add files via upload

---
+++ python_comments.py
@@ -0,0 +1,20 @@
+#This is a comment 
+print ( "Hello, world!")
+
+print ( "Hello, world!") #This is a comment 
+
+#print ( "Hello, world!")
+print ( "Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line 
+print ( "Hello, world!")
+
+"""
+This is a comment 
+written in
+more than just one line
+"""
+
+
---
+++ python_variables_names.py
@@ -0,0 +1,12 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+myVariableName = "John"
+
+MyVariableName = "John"
+
+my_variable_name = "John"
